fb79db2b6abcbaab404058180c2e752987b065e6
preisvladimir <168311798+preisvladimir@users.noreply.github.com>
2025-09-20 12:36:32 +0200

    +dto pro auth

---
+++ src/main/java/cz/stavbau/backend/security/auth/dto/LoginRequest.java
@@ -0,0 +1,6 @@
+package cz.stavbau.backend.security.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(@Email String email, @NotBlank String password) {}
